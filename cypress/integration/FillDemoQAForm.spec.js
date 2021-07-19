45a6e362964b5a1143c48b59e835623f43660b88
Raghvendra Gupta <raghvendra@Raghvendras-MacBook-Pro.local>
2021-07-19 14:12:55 +0530

    Code Refactored

--- cypress/integration/FillDemoQAForm.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Fill form on demoQA website",function(){
-    it('Fill Form Details',function(){
-        cy.visit('https://demoqa.com/automation-practice-form')
-        cy.get('#firstName').type('Raghav')
-        cy.get('#lastName').type('Gupta')
-    })
-})
-
