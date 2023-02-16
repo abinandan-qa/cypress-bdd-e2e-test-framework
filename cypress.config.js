04a3c84a5d00c37bf9750a7a50cfd66d4bb8b7b8
apreeti-tw <apreeti@thoughtworks.com>
2023-02-16 11:43:10 +0530

    Jaishree|Preeti - Add Multi Reports

--- cypress.config.js
+++ cypress.config.js
@@ -7,17 +7,22 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-      async setupNodeEvents(on, config) {
-        await preprocessor.addCucumberPreprocessorPlugin(on, config);
+    async setupNodeEvents(on, config) {
+      await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
-        on(
+      on(
           "file:preprocessor",browserify.default(config)
-        );
+      );
 
-        // Make sure to return the config object as it might have been modified by the plugin.
-        return config;
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
     },
     baseUrl: 'https://www.advantageonlineshopping.com/#/',
     specPattern: 'cypress/e2e/**/*.feature',
+  },
+  screenshotOnRunFailure: true,
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: 'reporter-config.json'
   },
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/index.js is processed and
+// This example support/e2e.js is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -15,6 +15,21 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from "mochawesome/addContext";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+//https://docs.cypress.io/api/events/catalog-of-events#docusaurus_skipToContent_fallback
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === "failed") {
+
+        //Adding screenshot to mochaawesome report
+        const screenshot= `../../screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`
+        addContext({ test }, screenshot);
+
+        //Adding video to mochaawesome report
+        const video= `../../videos/${Cypress.spec.name}.mp4`
+        addContext({ test }, video);
+    }
+})
